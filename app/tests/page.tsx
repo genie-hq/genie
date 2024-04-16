aee76ca7468d2046e184f37d8623b42fa733c99c
WMumei <luongwinter@gmail.com>
2024-04-16 17:40:55 +0700

    tests page v1.0

--- app/tests/page.tsx
+++ app/tests/page.tsx
@@ -5,7 +5,10 @@ import { data } from "./data/data";
 
 function page() {
   return (
-    <div className="p-2 md:p-6 lg:p-12 flex flex-col gap-2 bg-black w-full h-screen overflow-auto items-center">
+    <div className="p-2 md:p-6 flex flex-col gap-2 bg-black w-full h-screen overflow-auto items-center">
+      <h1 className="text-xl md:text-2xl flex items-center text-white md:m-2 lg:m-3">
+        Test for branch X
+      </h1>
       {data.map((_: any, i) => (
         <TestComponent testInfo={_} />
       ))}
