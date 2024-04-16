c66325014bd678e1ae7df7ccd03675dabab76c0f
Mudoker <huuquoc7603@gmail.com>
2024-04-17 00:15:37 +0700

    feat: allow to change chat title

--- app/ai_prompt/components/hamburger-menu.tsx
+++ app/ai_prompt/components/hamburger-menu.tsx
@@ -9,13 +9,37 @@ import plus_img from '../assets/plus.svg'
 
 export const HamburgerMenu = () => {
     const [messageList, setMessageList] = useState<string[]>([]);
+    const [editTitleIndex, setEditTitleIndex] = useState<number>(-1);
+    const [newTitle, setNewTitle] = useState<string>('')
 
     const createNewMessage = (title: string) => {
         setMessageList(prevMessages => [...prevMessages, title]);
     };
 
+    const onInputChange = (value: string) => {
+        setNewTitle(value);
+    };
+
+    const handleEditTitle = (newTitle: string, index: number) => {
+        setEditTitleIndex(-1)
+
+        if (newTitle === '') {
+
+            return
+        }
+
+        setMessageList(prevList =>
+            prevList.map((message, idx) =>
+                idx === index ? newTitle : message
+            )
+        );
+        setNewTitle("")
+    };
+
     return (
-        <div className='bg-black w-64 h-full flex flex-col justify-between px-3 text-white'>
+        <div
+            className='bg-black w-64 h-full flex flex-col justify-between px-3 text-white'
+        >
             <div>
                 <div className='flex justify-between font-semibold pt-6 items-center text-xl'>
                     <div className=' hover:cursor-pointer'>
@@ -29,7 +53,7 @@ export const HamburgerMenu = () => {
 
                 <Button
                     className='bg-white w-full my-6 text-black hover:bg-slate-200 items-center flex justify-between'
-                    onClick={() => createNewMessage("Hello")}
+                    onClick={() => createNewMessage("New Chat")}
                 >
                     Create new chat
 
@@ -52,7 +76,24 @@ export const HamburgerMenu = () => {
                         className='w-full my-0.5 px-2 text-white bg-transparent hover:bg-white/20 justify-start'
                     >
                         <div className="truncate flex justify-between w-full items-center">
-                            {message}
+                            {editTitleIndex === index ? (
+                                <input
+                                    type="text"
+                                    placeholder={message}
+                                    value={newTitle}
+                                    onChange={(e) => onInputChange(e.target.value)}
+                                    onKeyDown={(e) => {
+                                        if (e.key === 'Enter') {
+                                            handleEditTitle(newTitle, index);
+                                        }
+                                    }}
+                                    onBlur={() => handleEditTitle(newTitle, index)}
+                                    autoFocus
+                                    className='bg-transparent focus:outline-none'
+                                />
+                            ) : (
+                                <span>{message}</span>
+                            )}
 
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
@@ -62,6 +103,7 @@ export const HamburgerMenu = () => {
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
                                 className=' w-4 h-4 stroke-gray-500 hover:stroke-white'
+                                onClick={() => setEditTitleIndex(index)}
                             >
                                 <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
                                 <path d="M18.375 2.625a2.121 2.121 0 1 1 3 3L12 15l-4 1 1-4Z" />
