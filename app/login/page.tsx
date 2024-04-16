035cae87753146ffe963dddc37e0e644c348411f
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-04-16 22:36:32 +0700

    feat!: Genie title in log in page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -54,7 +54,9 @@ export default function Login({
   };
 
   return (
+  
     <div className="flex-1 flex flex-col w-full px-2 md:px-8 sm:max-w-md justify-center gap-2">
+      <h1 className="text-center text-6xl font-bold mb-7">Genie</h1>
       <Link
         href="/"
         className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
