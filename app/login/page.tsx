05f3f4a1fb5f05317c0e82537aa6197dbd67cff9
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-04-16 22:40:46 +0700

    feat: add Genie description below the title

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -56,7 +56,8 @@ export default function Login({
   return (
   
     <div className="flex-1 flex flex-col w-full px-2 md:px-8 sm:max-w-md justify-center gap-2">
-      <h1 className="text-center text-6xl font-bold mb-7">Genie</h1>
+      <h1 className="text-center text-6xl font-bold mb-1">Genie</h1>
+      <p className="text-center text-7 text-gray-500 mb-7">Welcome to Genie, AI-powered Test Generation.</p>
       <Link
         href="/"
         className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
