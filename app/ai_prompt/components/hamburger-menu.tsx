1994e01f2c8322641f285bfa9b9907fbd93c1299
Mudoker <huuquoc7603@gmail.com>
2024-04-17 10:43:23 +0700

    feat: scroll on many chats

--- app/ai_prompt/components/hamburger-menu.tsx
+++ app/ai_prompt/components/hamburger-menu.tsx
@@ -70,48 +70,51 @@ export const HamburgerMenu = () => {
 
                 <hr className="mb-2  border-white/30" />
 
-                {messageList.map((message, index) => (
-                    <Button
-                        key={index}
-                        className='w-full my-0.5 px-2 text-white bg-transparent hover:bg-white/20 justify-start'
-                    >
-                        <div className="truncate flex justify-between w-full items-center">
-                            {editTitleIndex === index ? (
-                                <input
-                                    type="text"
-                                    placeholder={message}
-                                    value={newTitle}
-                                    onChange={(e) => onInputChange(e.target.value)}
-                                    onKeyDown={(e) => {
-                                        if (e.key === 'Enter') {
-                                            handleEditTitle(newTitle, index);
-                                        }
-                                    }}
-                                    onBlur={() => handleEditTitle(newTitle, index)}
-                                    autoFocus
-                                    className='bg-transparent focus:outline-none'
-                                />
-                            ) : (
-                                <span>{message}</span>
-                            )}
+                <div className='overflow-auto h-96'>
+                    {messageList.map((message, index) => (
+                        <Button
+                            key={index}
+                            className='w-full my-0.5 px-2 text-white bg-transparent hover:bg-white/20 justify-start'
+                        >
+                            <div className="truncate flex justify-between w-full items-center">
+                                {editTitleIndex === index ? (
+                                    <input
+                                        type="text"
+                                        placeholder={message}
+                                        value={newTitle}
+                                        onChange={(e) => onInputChange(e.target.value)}
+                                        onKeyDown={(e) => {
+                                            if (e.key === 'Enter') {
+                                                handleEditTitle(newTitle, index);
+                                            }
+                                        }}
+                                        onBlur={() => handleEditTitle(newTitle, index)}
+                                        autoFocus
+                                        className='bg-transparent focus:outline-none'
+                                    />
+                                ) : (
+                                    <span>{message}</span>
+                                )}
 
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className=' w-4 h-4 stroke-gray-500 hover:stroke-white'
-                                onClick={() => setEditTitleIndex(index)}
-                            >
-                                <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
-                                <path d="M18.375 2.625a2.121 2.121 0 1 1 3 3L12 15l-4 1 1-4Z" />
-                            </svg>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className=' w-4 h-4 stroke-gray-500 hover:stroke-white'
+                                    onClick={() => setEditTitleIndex(index)}
+                                >
+                                    <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+                                    <path d="M18.375 2.625a2.121 2.121 0 1 1 3 3L12 15l-4 1 1-4Z" />
+                                </svg>
 
-                        </div>
-                    </Button>
-                ))}
+                            </div>
+                        </Button>
+                    ))}
+                </div>
+
             </div>
 
             <div className='mb-6'>
