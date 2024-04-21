1ece335ebfbc58f7908c1f447c86b0c348c799f3
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-04-21 23:59:46 +0700

    db: merge relevant migration files

--- supabase/migrations/20240421093933_new_migration.sql
+++
@@ -1,3 +0,0 @@
-alter table "public"."user_installation_info" alter column "installation_id" set data type character varying using "installation_id"::character varying;
-
-
--- supabase/migrations/20240421103744_new_migration.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."user_installation_info" drop column "expired_at";
-
-alter table "public"."user_installation_info" add column "expires_at" timestamp without time zone;
-
-
