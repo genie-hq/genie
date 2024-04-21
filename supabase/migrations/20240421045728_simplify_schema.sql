7b22ea1a760487c456d64dd5d7feeb139e296d00
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-04-21 11:58:00 +0700

    db: simplify schema

---
+++ supabase/migrations/20240421045728_simplify_schema.sql
@@ -0,0 +1,342 @@
+revoke delete on table "public"."branches" from "anon";
+
+revoke insert on table "public"."branches" from "anon";
+
+revoke references on table "public"."branches" from "anon";
+
+revoke select on table "public"."branches" from "anon";
+
+revoke trigger on table "public"."branches" from "anon";
+
+revoke truncate on table "public"."branches" from "anon";
+
+revoke update on table "public"."branches" from "anon";
+
+revoke delete on table "public"."branches" from "authenticated";
+
+revoke insert on table "public"."branches" from "authenticated";
+
+revoke references on table "public"."branches" from "authenticated";
+
+revoke select on table "public"."branches" from "authenticated";
+
+revoke trigger on table "public"."branches" from "authenticated";
+
+revoke truncate on table "public"."branches" from "authenticated";
+
+revoke update on table "public"."branches" from "authenticated";
+
+revoke delete on table "public"."branches" from "service_role";
+
+revoke insert on table "public"."branches" from "service_role";
+
+revoke references on table "public"."branches" from "service_role";
+
+revoke select on table "public"."branches" from "service_role";
+
+revoke trigger on table "public"."branches" from "service_role";
+
+revoke truncate on table "public"."branches" from "service_role";
+
+revoke update on table "public"."branches" from "service_role";
+
+revoke delete on table "public"."github_accounts" from "anon";
+
+revoke insert on table "public"."github_accounts" from "anon";
+
+revoke references on table "public"."github_accounts" from "anon";
+
+revoke select on table "public"."github_accounts" from "anon";
+
+revoke trigger on table "public"."github_accounts" from "anon";
+
+revoke truncate on table "public"."github_accounts" from "anon";
+
+revoke update on table "public"."github_accounts" from "anon";
+
+revoke delete on table "public"."github_accounts" from "authenticated";
+
+revoke insert on table "public"."github_accounts" from "authenticated";
+
+revoke references on table "public"."github_accounts" from "authenticated";
+
+revoke select on table "public"."github_accounts" from "authenticated";
+
+revoke trigger on table "public"."github_accounts" from "authenticated";
+
+revoke truncate on table "public"."github_accounts" from "authenticated";
+
+revoke update on table "public"."github_accounts" from "authenticated";
+
+revoke delete on table "public"."github_accounts" from "service_role";
+
+revoke insert on table "public"."github_accounts" from "service_role";
+
+revoke references on table "public"."github_accounts" from "service_role";
+
+revoke select on table "public"."github_accounts" from "service_role";
+
+revoke trigger on table "public"."github_accounts" from "service_role";
+
+revoke truncate on table "public"."github_accounts" from "service_role";
+
+revoke update on table "public"."github_accounts" from "service_role";
+
+revoke delete on table "public"."repositories" from "anon";
+
+revoke insert on table "public"."repositories" from "anon";
+
+revoke references on table "public"."repositories" from "anon";
+
+revoke select on table "public"."repositories" from "anon";
+
+revoke trigger on table "public"."repositories" from "anon";
+
+revoke truncate on table "public"."repositories" from "anon";
+
+revoke update on table "public"."repositories" from "anon";
+
+revoke delete on table "public"."repositories" from "authenticated";
+
+revoke insert on table "public"."repositories" from "authenticated";
+
+revoke references on table "public"."repositories" from "authenticated";
+
+revoke select on table "public"."repositories" from "authenticated";
+
+revoke trigger on table "public"."repositories" from "authenticated";
+
+revoke truncate on table "public"."repositories" from "authenticated";
+
+revoke update on table "public"."repositories" from "authenticated";
+
+revoke delete on table "public"."repositories" from "service_role";
+
+revoke insert on table "public"."repositories" from "service_role";
+
+revoke references on table "public"."repositories" from "service_role";
+
+revoke select on table "public"."repositories" from "service_role";
+
+revoke trigger on table "public"."repositories" from "service_role";
+
+revoke truncate on table "public"."repositories" from "service_role";
+
+revoke update on table "public"."repositories" from "service_role";
+
+revoke delete on table "public"."test_cases" from "anon";
+
+revoke insert on table "public"."test_cases" from "anon";
+
+revoke references on table "public"."test_cases" from "anon";
+
+revoke select on table "public"."test_cases" from "anon";
+
+revoke trigger on table "public"."test_cases" from "anon";
+
+revoke truncate on table "public"."test_cases" from "anon";
+
+revoke update on table "public"."test_cases" from "anon";
+
+revoke delete on table "public"."test_cases" from "authenticated";
+
+revoke insert on table "public"."test_cases" from "authenticated";
+
+revoke references on table "public"."test_cases" from "authenticated";
+
+revoke select on table "public"."test_cases" from "authenticated";
+
+revoke trigger on table "public"."test_cases" from "authenticated";
+
+revoke truncate on table "public"."test_cases" from "authenticated";
+
+revoke update on table "public"."test_cases" from "authenticated";
+
+revoke delete on table "public"."test_cases" from "service_role";
+
+revoke insert on table "public"."test_cases" from "service_role";
+
+revoke references on table "public"."test_cases" from "service_role";
+
+revoke select on table "public"."test_cases" from "service_role";
+
+revoke trigger on table "public"."test_cases" from "service_role";
+
+revoke truncate on table "public"."test_cases" from "service_role";
+
+revoke update on table "public"."test_cases" from "service_role";
+
+revoke delete on table "public"."test_cases_versions" from "anon";
+
+revoke insert on table "public"."test_cases_versions" from "anon";
+
+revoke references on table "public"."test_cases_versions" from "anon";
+
+revoke select on table "public"."test_cases_versions" from "anon";
+
+revoke trigger on table "public"."test_cases_versions" from "anon";
+
+revoke truncate on table "public"."test_cases_versions" from "anon";
+
+revoke update on table "public"."test_cases_versions" from "anon";
+
+revoke delete on table "public"."test_cases_versions" from "authenticated";
+
+revoke insert on table "public"."test_cases_versions" from "authenticated";
+
+revoke references on table "public"."test_cases_versions" from "authenticated";
+
+revoke select on table "public"."test_cases_versions" from "authenticated";
+
+revoke trigger on table "public"."test_cases_versions" from "authenticated";
+
+revoke truncate on table "public"."test_cases_versions" from "authenticated";
+
+revoke update on table "public"."test_cases_versions" from "authenticated";
+
+revoke delete on table "public"."test_cases_versions" from "service_role";
+
+revoke insert on table "public"."test_cases_versions" from "service_role";
+
+revoke references on table "public"."test_cases_versions" from "service_role";
+
+revoke select on table "public"."test_cases_versions" from "service_role";
+
+revoke trigger on table "public"."test_cases_versions" from "service_role";
+
+revoke truncate on table "public"."test_cases_versions" from "service_role";
+
+revoke update on table "public"."test_cases_versions" from "service_role";
+
+revoke delete on table "public"."test_libraries" from "anon";
+
+revoke insert on table "public"."test_libraries" from "anon";
+
+revoke references on table "public"."test_libraries" from "anon";
+
+revoke select on table "public"."test_libraries" from "anon";
+
+revoke trigger on table "public"."test_libraries" from "anon";
+
+revoke truncate on table "public"."test_libraries" from "anon";
+
+revoke update on table "public"."test_libraries" from "anon";
+
+revoke delete on table "public"."test_libraries" from "authenticated";
+
+revoke insert on table "public"."test_libraries" from "authenticated";
+
+revoke references on table "public"."test_libraries" from "authenticated";
+
+revoke select on table "public"."test_libraries" from "authenticated";
+
+revoke trigger on table "public"."test_libraries" from "authenticated";
+
+revoke truncate on table "public"."test_libraries" from "authenticated";
+
+revoke update on table "public"."test_libraries" from "authenticated";
+
+revoke delete on table "public"."test_libraries" from "service_role";
+
+revoke insert on table "public"."test_libraries" from "service_role";
+
+revoke references on table "public"."test_libraries" from "service_role";
+
+revoke select on table "public"."test_libraries" from "service_role";
+
+revoke trigger on table "public"."test_libraries" from "service_role";
+
+revoke truncate on table "public"."test_libraries" from "service_role";
+
+revoke update on table "public"."test_libraries" from "service_role";
+
+alter table "public"."branches" drop constraint "public_branches_repository_id_fkey";
+
+alter table "public"."github_accounts" drop constraint "github_accounts_username_key";
+
+alter table "public"."github_accounts" drop constraint "public_github_accounts_user_id_fkey";
+
+alter table "public"."repositories" drop constraint "public_repositories_github_account_id_fkey";
+
+alter table "public"."test_cases_versions" drop constraint "public_test_cases_versions_test_case_id_fkey";
+
+alter table "public"."test_cases_versions" drop constraint "public_test_cases_versions_test_file_version_id_fkey";
+
+alter table "public"."test_files" drop constraint "public_test_files_branch_id_fkey";
+
+alter table "public"."test_libraries" drop constraint "test_libraries_name_key";
+
+alter table "public"."branches" drop constraint "branches_pkey";
+
+alter table "public"."github_accounts" drop constraint "github_accounts_pkey";
+
+alter table "public"."repositories" drop constraint "repositories_pkey";
+
+alter table "public"."test_cases" drop constraint "test_cases_pkey";
+
+alter table "public"."test_cases_versions" drop constraint "test_cases_versions_pkey";
+
+alter table "public"."test_libraries" drop constraint "test_libraries_pkey";
+
+drop index if exists "public"."branches_pkey";
+
+drop index if exists "public"."github_accounts_pkey";
+
+drop index if exists "public"."github_accounts_username_key";
+
+drop index if exists "public"."repositories_pkey";
+
+drop index if exists "public"."test_cases_pkey";
+
+drop index if exists "public"."test_cases_versions_pkey";
+
+drop index if exists "public"."test_libraries_name_key";
+
+drop index if exists "public"."test_libraries_pkey";
+
+drop table "public"."branches";
+
+drop table "public"."github_accounts";
+
+drop table "public"."repositories";
+
+drop table "public"."test_cases";
+
+drop table "public"."test_cases_versions";
+
+drop table "public"."test_libraries";
+
+alter table "public"."test_files" drop column "branch_id";
+
+alter table "public"."test_files" drop column "language";
+
+alter table "public"."test_files" drop column "path";
+
+alter table "public"."test_files" drop column "test_library_id";
+
+alter table "public"."test_files" drop column "updated_at";
+
+alter table "public"."test_files" add column "branch" text not null;
+
+alter table "public"."test_files" add column "file_path" text not null;
+
+alter table "public"."test_files" add column "github_username" text not null;
+
+alter table "public"."test_files" add column "repository" text not null;
+
+alter table "public"."test_files" add column "target_branch" text not null;
+
+alter table "public"."test_files" add column "test_framework" text not null;
+
+alter table "public"."test_files" add column "test_library" text not null;
+
+alter table "public"."test_files" alter column "created_at" drop not null;
+
+alter table "public"."test_files" alter column "created_at" set data type timestamp with time zone using "created_at"::timestamp with time zone;
+
+alter table "public"."test_files" alter column "name" drop not null;
+
+alter table "public"."test_files" disable row level security;
+
+
+
