7114e12e1d7b4563f3a50e799bfed1920d5d255b
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-04-21 13:42:33 +0700

    feat!: get installation token

---
+++ app/api/github/apps/callback/route.ts
@@ -0,0 +1,70 @@
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+import { Octokit } from 'octokit';
+import { createAppAuth } from '@octokit/auth-app';
+import fs from 'fs';
+
+export const dynamic = 'force-dynamic';
+
+export async function GET(req: Request) {
+  // Extract installation ID and error details from query parameters
+  const urlParams = new URLSearchParams(req.url.split('?')[1]);
+  console.log('urlParams', urlParams);
+  const installationId = urlParams.get('installation_id');
+  const error = urlParams.get('error');
+  const errorDescription = urlParams.get('error_description');
+
+  // TODO: Retrieve the userId
+  const userId = '';
+
+  // Handle installation failure
+  if (installationId == null) {
+    if (error) {
+      console.error(`Installation failed: ${error} - ${errorDescription}`);
+      // Redirect to a more informative error page
+      return NextResponse.redirect(
+        `https://intelligenie.vercel.app/installation-failed?error=${error}&description=${errorDescription}`
+      );
+    } else {
+      // Generic redirect if no specific error information is available
+      return NextResponse.redirect(
+        `https://intelligenie.vercel.app/installation-failed`
+      );
+    }
+  }
+  // Obtain installation access token using installation ID
+  const accessToken = await getAccessToken(installationId);
+
+  await updateUserInstallationToken(userId, installationId, accessToken);
+
+  // Redirect to the installation page of your GitHub app
+  return NextResponse.redirect(`https://intelligenie.vercel.app/new`);
+}
+
+async function getAccessToken(installationId: string) {
+  // Ensure your installation ID is a number
+  const idNumber = parseInt(installationId, 10);
+  if (isNaN(idNumber)) {
+    throw new Error('Installation ID must be a number.');
+  }
+
+  // Initialize Octokit with app authentication
+  const auth = createAppAuth({
+    appId: process.env.BOT_APP_ID || '', // Corrected to BOT_APP_ID
+    privateKey: process.env.BOT_PRIVATE_KEY?.replace(/\\n/g, '\n') || '', // Ensure newlines are properly formatted
+    installationId: idNumber,
+  });
+
+  // Retrieve the installation access token
+  const installationAccessToken = await auth({ type: 'installation' });
+  return installationAccessToken.token;
+}
+
+// TODO: Store it in db
+async function updateUserInstallationToken(
+  userId: String,
+  installationId: String,
+  accessToken: String
+) {
+  // Implement logic to store installation ID & accessToken in Supabase users table
+}
