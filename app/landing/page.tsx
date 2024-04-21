12ec30759702b7811722f39e3788e50fbda085a7
Mudoker <huuquoc7603@gmail.com>
2024-04-22 03:23:05 +0700

    style!: update ui

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,36 +1,88 @@
 import React from 'react';
 import Image from 'next/image';
+import bright_logo from './bright-light.png';
+import tutu3ru_logo from './Transparent.png';
+import coding_img from './coding.png';
 
 function page() {
   return (
-    <div className="flex gap-12 p-4 h-full items-center relative">
-      <div className="flex flex-col w-full md:w-1/2 pl-4 md:pl-16 gap-4 md:gap-6 tracking-tight z-10 shrink-0">
-        <h1 className="text-3xl md:text-5xl">
-          Automated Test Case Generation with CI/CD
-        </h1>
-        <h2 className="text-md md:text-lg ">
-          Enjoy a seamless experience with our automated test case generation.
-        </h2>
-        <div className="flex gap-4">
-          <button className="relative inline-flex items-center justify-center p-0.5 overflow-hidden  font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-              Get started
+    <div className='w-full h-full flex flex-col justify-center items-center'>
+      <div className="flex w-full items-center justify-center gap-2">
+        <div className="flex flex-col w-full md:w-1/2 md:pl-16 gap-4 md:gap-8">
+          <h1 className='text-lg md:text-2xl font-semibold'>
+            <span
+              className='overflow-hidden bg-gradient-to-r bg-clip-text font-bold text-transparent dark:from-pink-300 dark:via-amber-200 dark:to-blue-300 from-pink-600 via-purple-500 to-sky-500'>
+              Intelligenie.ai
             </span>
-          </button>
-          <button className="flex items-center">It's free</button>
+          </h1>
+
+          <h1 className="text-3xl md:text-4xl font-bold max-w-96">
+            Automated Test Case Generation with CI/CD
+          </h1>
+
+          <div className='flex flex-col max-w-md gap-2 text-lg'>
+            <div>
+              "Skip test file creation. Genie automates it.
+            </div>
+              Focus on bigger tasks."
+
+            <div className='font-semibold text-sm mr-6 mt-4'>
+              Genie Team - 2024
+            </div>
+
+            <div className='font-bold text-sm mr-6'>
+            </div>
+          </div>
+
+          <div className="flex gap-4">
+            <button className="relative inline-flex items-center justify-center p-0.5 overflow-hidden  font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Get started
+              </span>
+            </button>
+            <button className="flex items-center">It's free</button>
+          </div>
         </div>
-      </div>
-      <div className="absolute opacity-30 md:opacity-100 md:relative bottom-0 right-0 w-full h-full p-12 flex items-center">
+
         <Image
-          src="/decorative.png"
+          src={coding_img}
           // src="/decorative.png"
           className="object-contain"
-          height={500}
-          width={500}
+          height={450}
+          width={450}
           alt="Decorative"
         />
       </div>
+
+      <div className='flex flex-col items-center justify-center'>
+        <div className='font-semibold opacity-60 text-lg'>
+          Trusted By
+        </div>
+
+        <div className='flex gap-14 justify-center items-center mt-8'>
+          <div>
+            <Image
+              src={bright_logo}
+              height={70}
+              width={70}
+              alt="sun"
+            />
+          </div>
+
+
+          <div className='w-14 h-14'>
+            <Image
+              src={tutu3ru_logo}
+              height={70}
+              width={70}
+              alt="sun"
+            />
+          </div>
+
+        </div>
+      </div>
     </div>
+
   );
 }
 
