058632df2d990f243956c82aaec714976a71f2a8
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-04-22 06:35:23 +0700

    feat: move landing page to root

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,15 +6,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <div className="h-full p-4 md:px-8 lg:px-16 xl:px-32">
       <div className="flex gap-2 items-center mb-4">
-        <Link href="/dashboard">
-          <Button className="rounded-full">Dashboard</Button>
-        </Link>
-        <Button className="rounded-full" disabled>
-          Test Files
-        </Button>
-        <Button className="rounded-full" disabled>
-          Test Cases
-        </Button>
         <Link href="/dashboard/github">
           <Button className="rounded-full">GitHub</Button>
         </Link>
